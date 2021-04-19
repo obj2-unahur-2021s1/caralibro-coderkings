56e7a8c6d27d91901b053bc9ecc7ff9c8c6232e0
Cesar Marca <cesar.marca.20@hotmail.com>
2021-04-18 23:08:00 -0300

    Se agrego fun de video y test

--- src/main/kotlin/ar/edu/unahur/obj2/caralibro/Publicacion.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/caralibro/Publicacion.kt
@@ -14,3 +14,25 @@ class Foto(val alto: Int, val ancho: Int) : Publicacion() {
 class Texto(val contenido: String) : Publicacion() {
   override fun espacioQueOcupa() = contenido.length
 }
+
+
+// opcion 1 = calidad SD
+// opcion 2 = calidad 720HD
+// opcion 3 = calidad 1080HD
+class Video(val duracion: Int, var calidad: String) :Publicacion() {
+  override fun espacioQueOcupa(): Int {
+    if (calidad == "SD") {
+      return duracion
+    } else if (calidad == "720") {
+ //   720p HD
+      return duracion * 3
+    } else {
+ //  1080p HD
+      return duracion * 6
+    }
+  }
+
+  fun modificarCalidad(nuevaCalidad: String) {
+    calidad = nuevaCalidad
+  }
+}
--- src/test/kotlin/ar/edu/unahur/obj2/caralibro/UsuarioTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/caralibro/UsuarioTest.kt
@@ -8,26 +8,48 @@ class UsuarioTest : DescribeSpec({
     val saludoCumpleanios = Texto("Felicidades Pepito, que los cumplas muy feliz")
     val fotoEnCuzco = Foto(768, 1024)
 
+    val videoSD = Video(10,"720")
+    val video720 = Video(10,"720")
+    val video1080 = Video(10,"1080")
+
+
     describe("Una publicación") {
       describe("de tipo foto") {
         it("ocupa ancho * alto * compresion bytes") {
           fotoEnCuzco.espacioQueOcupa().shouldBe(550503)
         }
       }
-
       describe("de tipo texto") {
         it("ocupa tantos bytes como su longitud") {
           saludoCumpleanios.espacioQueOcupa().shouldBe(45)
         }
       }
-    }
 
+
+      describe("de tipo video" ) {
+        it("calidad SD") {
+          videoSD.modificarCalidad("SD")
+          videoSD.espacioQueOcupa().shouldBe(10)
+        }
+        it("calidad HD 720") {
+          video720.espacioQueOcupa().shouldBe(30)
+        }
+        it("calidad HD 1080") {
+          video1080.espacioQueOcupa().shouldBe(60)
+        }
+      }
+
+    }
+//ARREGLAR EL TOTAL
     describe("Un usuario") {
       it("puede calcular el espacio que ocupan sus publicaciones") {
         val juana = Usuario()
         juana.agregarPublicacion(fotoEnCuzco)
         juana.agregarPublicacion(saludoCumpleanios)
-        juana.espacioDePublicaciones().shouldBe(550548)
+        juana.agregarPublicacion(videoSD)
+        juana.agregarPublicacion(video720)
+        juana.agregarPublicacion(video1080)
+        juana.espacioDePublicaciones().shouldBe(550648)
       }
     }
   }
